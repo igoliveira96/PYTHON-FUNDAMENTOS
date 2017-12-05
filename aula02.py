ecb19fa050613b56ec9c2466ba00ae8e46f90db4
Igor Goulart de Oliveira <igoliveira@uenp.edu.br>
2017-12-05 21:57:42 -0200

    Aula 2

---
+++ aula02.py
@@ -0,0 +1,15 @@
+#Tipos primitivos e saída de dados
+
+"""
+numa = int(input('Digite o primeiro número: '))
+numb = int(input('Digite o seugundo número: '))
+soma = numa + numb
+print('A soma entre {} e {} é {}'.format(numa, numb, soma))
+"""
+
+#Tipos primitivos: int float bool str
+
+num = input('Digite algo: ')
+print(num.isnumeric())
+print(num.isalpha())
+print(num.isalnum())
