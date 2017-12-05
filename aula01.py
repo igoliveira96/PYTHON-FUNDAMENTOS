c3e5b13b143e04b534c7b93bce5117205cd4a14e
Igor Goulart de Oliveira <igoliveira@uenp.edu.br>
2017-12-05 21:10:26 -0200

    Aula 1 de Python

---
+++ aula01.py
@@ -0,0 +1,44 @@
+#Imprimir uma mensagem:
+"""
+print('Olá, Mundo!')
+"""
+
+#Variáveis:
+"""
+nome = 'Igor Goulart de Oliveira'
+idade = 21
+peso = 63.4
+
+print(nome, idade, peso)
+"""
+
+#Coleta de valores:
+"""
+nome = input('Qual o seu nome? ')
+idade = input('Qual é a sua idade? ')
+peso = input('Qual é o seu peso? ')
+
+print(nome, idade, peso)
+"""
+
+#Desafio 1:
+"""
+print('Desafio 01')
+nome = input('Qual o seu nome? ')
+print('Bem-Vindo '+nome)
+"""
+
+#Desafio 2:
+"""
+print('Desafio 02')
+dia = input('Dia = ')
+mes = input('Mês = ')
+ano = input('Ano = ')
+print('Você nasceu no dia', dia, 'de', mes, 'de', ano)
+"""
+#Desafio 3:
+print('Desafio 03')
+numa = int(input('Digite o primeiro número: '))
+numb = int(input('Digite o segundo número: '))
+soma = numa + numb
+print('A soma é: {}'.format(soma))
